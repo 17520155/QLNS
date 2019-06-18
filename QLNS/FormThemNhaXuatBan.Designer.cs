6078e2351f3b9834791216be5588fb9fd53ee35c
17520155 <17520155@gm.uit.edu.vn>
2019-06-18 10:12:44 +0700

    17520155 Test Font ThemNhaXB

--- QLNS/FormThemNhaXuatBan.Designer.cs
+++ QLNS/FormThemNhaXuatBan.Designer.cs
@@ -55,7 +55,7 @@
             // labelTenNhaXuatBan
             // 
             this.labelTenNhaXuatBan.AutoSize = true;
-            this.labelTenNhaXuatBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTenNhaXuatBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.labelTenNhaXuatBan.Location = new System.Drawing.Point(83, 79);
             this.labelTenNhaXuatBan.Name = "labelTenNhaXuatBan";
             this.labelTenNhaXuatBan.Size = new System.Drawing.Size(137, 20);
